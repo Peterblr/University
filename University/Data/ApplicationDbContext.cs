631e8da118c3979a33a37f3e784505e905f861eb
DESKTOP-7HUNCQ5\user <piotr.sviryd@aisnovations.com>
2022-08-13 17:09:42 +0300

    Create the database context

--- University/Data/ApplicationDbContext.cs
+++ University/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using University.Models;
 
 namespace University.Data
 {
@@ -7,7 +8,17 @@ namespace University.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
+        { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Course>().ToTable("Course");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
+            modelBuilder.Entity<Student>().ToTable("Student");
         }
     }
 }
